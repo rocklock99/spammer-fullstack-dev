9d5afb651baff58ed4930443f1410235591cf80f
rocklock99 <143745868+rocklock99@users.noreply.github.com>
2023-12-16 16:08:16 -0800

    ...

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 //import NewPostForm from "@/components/NewPostForm.jsx";
 import Posts from "@/components/Posts.jsx";
+import { prisma } from "@/lib/api.js";
 
 export default function Home() {
   return (
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -17,17 +17,42 @@ import Post from "./Post.jsx";
 //   );
 // }
 
-export default async function Posts() {
-  let posts = [];
-  const response = await fetch("/api/posts");
-  const postsResponse = await response.json();
-  if (postsResponse.success) {
-    console.log("Initial posts retrieval succeeded.");
-    posts = postsResponse.posts;
-    console.log(posts);
-  } else {
-    console.log("Initial posts retrieval failed.");
-  }
+// export default async function Posts() {
+//   let posts = [];
+//   const response = await fetch(`http://localhost:3000/api/posts`);
+//   const postsResponse = await response.json();
+//   if (postsResponse.success) {
+//     console.log("Initial posts retrieval succeeded.");
+//     posts = postsResponse.posts;
+//     console.log(posts);
+//   } else {
+//     console.log("Initial posts retrieval failed.");
+//   }
+
+// export async function getServerSideProps() {
+//   try {
+//     const posts = await prisma.post.findMany({
+//       orderBy: { createdAt: "desc" },
+//     });
+
+//     console.log("Fetched posts:", posts); // Log fetched posts
+
+//     return {
+//       props: { posts },
+//     };
+//   } catch (error) {
+//     console.error("Error in getServerSideProps:", error);
+//     return {
+//       props: { posts: [] }, // Return an empty array if there's an error
+//     };
+//   }
+// }
+export default async function Feed() {
+  const posts = await prisma.post.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
   return (
     <div id="posts-container">
